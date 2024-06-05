a118770b1f654630dfb05a6998cbd3f217eab280
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-06-05 23:01:41 +0600

    feat: Add order controller and services
    The code changes introduce a new order controller and services for creating and retrieving orders. This will allow users to place orders for products and view their order history.
    
    Based on the recent user commits and repository commits, the commit message follows the established convention of starting with a type (feat for a new feature) and providing a concise description of the changes made.

---
+++ src/app/modules/Orders/order.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import { ApiResponse } from '../../utils/ApiResponse';
+import Product from '../Products/product.model';
+import Order from './order.model';
+
+const createOrder = async (req: Request, res: Response) => {
+  try {
+    const { productId, quantity } = req.body;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Product not found' });
+    }
+
+    if (product.inventory.quantity < quantity) {
+      return res.status(400).json({
+        success: false,
+        message: 'Insufficient quantity available in inventory',
+      });
+    }
+
+    product.inventory.quantity -= quantity;
+    product.inventory.inStock = product.inventory.quantity > 0;
+    await product.save();
+
+    const order = new Order(req.body);
+    await order.save();
+
+    res
+      .status(201)
+      .json(new ApiResponse(201, order, 'Order created successfully'));
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Server error', error });
+  }
+};
+
+const getAllOrders = async (req: Request, res: Response) => {
+  const email = req.query.email as string;
+  console.log(email);
+
+  if (email) {
+    try {
+      const orders = await Order.find({ email });
+      res
+        .status(200)
+        .json(new ApiResponse(200, orders, 'Orders fetched successfully!'));
+    } catch (error) {
+      res.status(500).json({ success: false, message: 'Server error', error });
+    }
+  } else {
+    try {
+      const orders = await Order.find();
+      res
+        .status(200)
+        .json(new ApiResponse(200, orders, 'Orders fetched successfully!'));
+    } catch (error) {
+      res.status(500).json({ success: false, message: 'Server error', error });
+    }
+  }
+};
+
+export const orderServices = {
+  createOrder,
+  getAllOrders,
+};
